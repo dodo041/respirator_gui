45a4fc33cb5a92656422637fabee1eaeb5c87c53
Dominik Terze <dodo041@hotmail.de>
2022-01-03 01:57:01 +0100

    Sensor data model and signals

---
+++ src/model/sensorDataModel.py
@@ -0,0 +1,102 @@
+from typing import Deque
+from collections import deque
+from PySide6.QtCore import QObject, Signal
+
+
+# TODO somehow use this class to abstract signals from actual model
+class SensorModelSignals(QObject):
+    """
+    SensorModelSignals shall be used to inform controller/view about changes in the sensor data models.
+    """
+
+    modified_air_pressure_data = Signal
+    modified_air_temp_data = Signal
+    modified_animal_temp_data = Signal
+    modified_heatbed_temp_data = Signal
+    modified_eTVOC_data = Signal
+    modified_eCO2_data = Signal
+
+    def __init__(self):
+        super(SensorModelSignals, self).__init__()
+
+        self.modified_air_pressure_data = Signal(float)
+        self.modified_air_temp_data = Signal(float)
+        self.modified_animal_temp_data = Signal(float)
+        self.modified_heatbed_temp_data = Signal(float)
+        self.modified_eTVOC_data = Signal(float)
+        self.modified_eCO2_data = Signal(float)
+
+
+class SensorDataModel(QObject):
+
+    # Signals
+    modified_air_pressure_data = Signal(float)
+    modified_air_temp_data = Signal(float)
+    modified_animal_temp_data = Signal(float)
+    modified_heatbed_temp_data = Signal(float)
+    modified_eTVOC_data = Signal(float)
+    modified_eCO2_data = Signal(float)
+    modified_relative_humidity_data = Signal(float)
+
+    # Data queues
+    air_pressure_data = Deque
+    air_temp_data = Deque
+    animal_temp_data = Deque
+    heatbed_temp_data = Deque
+    eCO2_data = Deque
+    eTVOC_data = Deque
+    relative_humidity_data = Deque
+
+    min_pressure_border = float
+    max_pressure_border = float
+
+    def __init__(self):
+        """
+        The sensor data model consists of independent queues (of type collection.deque). That way reading one sensor
+        value from the queue does not depend on another value, which was recorded at the same time instance.
+        """
+        super(SensorDataModel, self).__init__()
+
+        # Queues (dequeues) with sensor data
+        self.air_pressure_data = deque()
+        self.air_temp_data = deque()
+        self.animal_temp_data = deque()
+        self.heatbed_temp_data = deque()
+        self.eCO2_data = deque()
+        self.eTVOC_data = deque()
+        self.relative_humidity_data = deque()
+
+        # Borders for pressure alarm
+        self.min_pressure_border = float()
+        self.max_pressure_border = float()
+
+    # Methods for writing data to specific sensor model
+    # TODO maybe there's a more elegant way of doing this to avoid code duplication? e.g. binding signals to model?
+
+    def write_air_pressure_data(self, data: float):
+        self.air_pressure_data.append(data)
+        self.modified_air_pressure_data.emit(data)
+
+    def write_air_temp_data(self, data: float):
+        self.air_temp_data.append(data)
+        self.modified_air_temp_data.emit(data)
+
+    def write_animal_temp_data(self, data: float):
+        self.animal_temp_data.append(data)
+        self.modified_animal_temp_data.emit(data)
+
+    def write_heatbed_temp_data(self, data: float):
+        self.heatbed_temp_data.append(data)
+        self.modified_heatbed_temp_data.emit(data)
+
+    def write_eTVOC_data(self, data: float):
+        self.eTVOC_data.append(data)
+        self.modified_eTVOC_data.emit(data)
+
+    def write_eCO2_data(self, data: float):
+        self.eCO2_data.append(data)
+        self.modified_eCO2_data.emit(data)
+
+    def write_relative_humidity_data(self, data: float):
+        self.relative_humidity_data.append(data)
+        self.modified_relative_humidity_data.emit(data)
