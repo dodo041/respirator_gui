6f070f0b5836999d1ec5cfeebd9ca179b7809b1a
Dominik Terze <dodo041@hotmail.de>
2022-01-31 00:23:51 +0100

    Refactored NumericalInstrument class
    - Added wrapper around inner elements for later use
    - Reimplemented PyQt sizeHint as size reference for dynamical resizing
    - Introduced size policies for space and size management
    - Added boders as visual hint during debugging

--- src/view/instrumentView.py
+++ src/view/instrumentView.py
@@ -1,5 +1,5 @@
 from PySide6.QtCore import QSize, Slot
-from PySide6.QtWidgets import QLCDNumber, QLabel, QVBoxLayout, QWidget
+from PySide6.QtWidgets import QLCDNumber, QLabel, QVBoxLayout, QWidget, QSizePolicy
 
 
 class NumericalInstrument(QWidget):
@@ -7,36 +7,74 @@ class NumericalInstrument(QWidget):
     Instrument widget which displays numerical values in LCD display style with a text label.
     """
 
-    _instrument_size = QSize(200, 100)
+    _min_width = 200
     _decimal_places = 5
-    _value = 0
-    lcd = QLCDNumber
+    _lcd = QLCDNumber
+    _instrument_title = str
 
-    def __init__(self, instrumentLabel: str):
+    def __init__(self, instrument_label: str):
         super(NumericalInstrument, self).__init__()
-        self._build_numerical_instrument(instrumentLabel)
+        self._build_numerical_instrument(instrument_label)
 
-    def _build_numerical_instrument(self, instrumentLabel: str) -> None:
+        # Set border around widget and inner elements as visual hint during debugging
+        if __debug__:
+            self.setStyleSheet("border: 1px solid blue;")
+            self._lcd.setStyleSheet("border: 1px solid red;")
+            self._instrument_title.setStyleSheet("border: 1px solid green;")
+
+    def _build_numerical_instrument(self, instrument_label: str) -> None:
         """
         Build a numerical LCD instrument.
 
-        :param instrumentLabel: Description to be displayed
+        :param instrument_label: Description to be displayed
         :return: None
         """
-        widget = QWidget(self)
-        layout = QVBoxLayout(widget)
-        title = QLabel(instrumentLabel)
-        self.lcd = QLCDNumber(self._decimal_places, self)
+        # Wrapper for inner contents of the NumericalInstrument (might be handy for later extension)
+        wrap_layout = QVBoxLayout(self)
+        wrap = QWidget()
+        wrap_layout.addWidget(wrap)
+        # Inner layout with actual contents
+        inner_layout = QVBoxLayout(wrap)
 
+        self._instrument_title = QLabel(instrument_label)
+        self._lcd = QLCDNumber(self._decimal_places)
         # Configure LCD widget
-        self.lcd.setSegmentStyle(self.lcd.Flat)
-        self.lcd.setSmallDecimalPoint(True)
-        self.lcd.setFixedSize(self._instrument_size)
-        self.lcd.display(self._value)
+        self._lcd.setSegmentStyle(self._lcd.Flat)
+        self._lcd.setSmallDecimalPoint(True)
 
-        layout.addWidget(self.lcd)
-        layout.addWidget(title)
+        inner_layout.addWidget(self._lcd)
+        inner_layout.addWidget(self._instrument_title)
+
+        self._set_size_policies()
+
+    def sizeHint(self) -> QSize:
+        """
+        Reimplementation of PyQt sizeHint.
+
+        :return: Preferred size of the NumericalInstrument widget.
+        """
+        return QSize(self._min_width, int(self._min_width * 0.6))
+
+    def _set_size_policies(self) -> None:
+        """
+        Set the PyQt size policies for the NumericalInstrument widget.
+
+        :return: None
+        """
+        # LCD display shall expand as much as possible in vertical direction
+        lcd_sp = QSizePolicy(QSizePolicy.Minimum, QSizePolicy.MinimumExpanding)
+        self._lcd.setSizePolicy(lcd_sp)
+
+        # NumericalInstrument widget and its contents shall not exceed the given size hint
+        num_instr_sp = QSizePolicy(QSizePolicy.Maximum, QSizePolicy.Maximum)
+        self.setSizePolicy(num_instr_sp)
 
     @Slot(float)
-    def on_modified_data(self, value: float):
-        self.lcd.display(value)
+    def on_modified_data(self, value: float) -> None:
+        """
+        Update value on LCD display.
+
+        :param value: Data emitted from signal
+        :return: None
+        """
+        self._lcd.display(value)
